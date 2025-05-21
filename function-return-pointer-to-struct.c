a73798a3f9f3d8d9e5a3521f779b6f8b8d910740
Arda Taşkın <152263771+ardataskinn@users.noreply.github.com>
2025-05-21 22:06:32 +0300

    Create function-return-pointer-to-struct.c

---
+++ function-return-pointer-to-struct.c
@@ -0,0 +1,28 @@
+struct rectangular {
+    float x;
+    float y;
+};
+
+struct polar {
+    float r;
+    float theta;
+};
+
+struct polar* convert(struct rectangular kare) {
+    struct polar *polarlik;
+    polarlik = (struct polar*)malloc(sizeof(struct polar));
+
+    (*polarlik).r = sqrt(((kare.x)*(kare.x) + (kare.y)*(kare.y)));
+    (*polarlik).theta = atan2f(kare.y,kare.x);
+    return polarlik;
+}
+
+int main() 
+{ 
+    struct rectangular karem = {3,4};   
+    struct polar *kalem = convert(karem);
+
+    printf("%f",(*kalem).r);
+    free(kalem);
+
+}
